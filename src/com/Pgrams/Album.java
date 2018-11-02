b18ac867a50749b3f007ded1ab46f86568de982f
posopos <pothonos42@gmail.com>
2018-11-02 14:37:47 +0000

    added addToPlaylist

--- src/com/Pgrams/Album.java
+++ src/com/Pgrams/Album.java
@@ -1,6 +1,7 @@
 package com.Pgrams;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class Album {
     private String name;
@@ -13,19 +14,39 @@ public class Album {
         this.songs = new ArrayList<Song>();
     }
 
-    public boolean addSong(String title, double duration){
-        if(findSong(title) == null) {
+    public boolean addSong(String title, double duration) {
+        if (findSong(title) == null) {
             this.songs.add(new Song(title, duration));
             return true;
         }
         return false;
     }
-    private Song findSong(String title){
-        for (Song checkSong : this.songs){
-            if (checkSong.getTitle().equals(title)){
+
+    private Song findSong(String title) {
+        for (Song checkSong : this.songs) {
+            if (checkSong.getTitle().equals(title)) {
                 return checkSong;
             }
         }
         return null;
+    }
+
+    public boolean addToPlaylist(int trackNumber, LinkedList<Song> playList) {
+        int index = trackNumber - 1;
+        if (index >= 0 && index <= this.songs.size()) {
+            playList.add(this.songs.get(index));
+            return true;
+        }
+        System.out.println("This album does not have a track " + trackNumber);
+        return false;
+    }
+    public boolean addToPlaylist(String title, LinkedList<Song> playList){
+        Song checkSong = findSong(title);
+        if (checkSong != null){
+            playList.add(checkSong);
+            return true;
+        }
+        System.out.println("Song " + title + " is not in this album");
+        return true;
     }
 }
